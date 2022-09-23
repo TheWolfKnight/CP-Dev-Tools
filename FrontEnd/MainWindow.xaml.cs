140d5c16d883e5668c785a7d88b1e7c239355b52
TheWolfKnight <philipalexknudsen@gmail.com>
2022-09-23 14:09:58 +0200

    Change the WPF from being made like a form application, to it being made like an WPF application

--- FrontEnd/MainWindow.xaml.cs
+++ FrontEnd/MainWindow.xaml.cs
@@ -25,22 +25,5 @@ namespace CP_Dev_Tools
             InitializeComponent();
         }
 
-        private void New_Click(object sender, RoutedEventArgs e) { }
-        private void Open_Click(object sender, RoutedEventArgs e) { }
-        private void RecentFiles_Click(object sender, RoutedEventArgs e) { }
-        private void Save_Click(object sender, RoutedEventArgs e) { }
-        private void SaveAs_Click(object sender, RoutedEventArgs e) { }
-        private void Import_Click(object sender, RoutedEventArgs e) { }
-        private void Export_Click(object sender, RoutedEventArgs e) { }
-
-        /// <summary>
-        /// Closes the program
-        /// </summary>
-        /// <param name="sender"> the object that called this function </param>
-        /// <param name="e"> the arguments of the call </param>
-        private void Exit_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
     }
 }
